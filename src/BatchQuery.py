64aa4efb207997e7cbfcdc09a33a30088c46e897
lawrencechen <lawrencechen@ixensor.com>
2020-03-29 23:01:21 +0800

    1. PageParser.py: finish basic flow. 2. BatchQuery: add Search, Limit, Sort classes. git push

--- src/BatchQuery.py
+++ src/BatchQuery.py
@@ -8,12 +8,91 @@ import requests
 
 #### Parent Class
 class APIAdaptor:
+    api_root = 'https://api.fda.gov/'
+    api_params = {'search', 'sort', 'count', 'limit', 'skip'}
+    api_warning = ('Search not assigned', 'Limit not assigned', 'Sort not assigned')
+
+    @classmethod
+    def warning_msg(cls, w_num):
+        print('API Warning: ', cls.api_warning[w_num])
+
     def init(self):
         # core list, defining type of APIs
-        self._api_type_list = {}
-    
+        self.endpoint_type = {'device/510k'}
+        self.syntax_keys = {'end_point_end': '.json?', 
+                            'param_asign': '=',
+                            'mediator': ':',
+                            'and': '&'}
+
+    class Search:
+        def __init__(self):
+            self._search_query = 'search='
+            self._searched = False
+        
+        @staticmethod
+        def basic_search(field, term):
+            return str(field) + ':\"' + str(term) + '\"'
+
+        def simple_search(self, field, term):
+            self._search_query = self.basic_search(field, term)
+            self._searched = True
+            return self
+        
+        def add_search(self, field, term, match = False):
+            if self._search_query == '':
+                self.simple_search(field, term)
+
+            elif match:
+                self._search_query += ('+AND+' + self.basic_search(field, term))
+            
+            else:
+                self._search_query += ('+' + self.basic_search(field, term))
+            self._searched = True
+            return self
+            
+        def return_search(self):
+            if (not self._searched):
+                APIAdaptor.warning_msg(0)
+                return
+            return self._search_query
+            
+    class Limit:
+        def __init__(self):
+            self._limit_num = None
+        
+        def set_limit_num(self, num = 1):
+            
+            self._limit_num = num
+            return self
+        
+        def return_limit(self):
+            if (self._limit_num is not None):
+                return 'limit=' + str(self._limit_num)
+            
+            else:
+                APIAdaptor.warning_msg(1)
+
+    class Sort:
+        sort_fields = {'1':'report_date'} # unfinished
+        sort_type = {'1': 'desc', '2': 'asc'}
+        def __init__(self):
+            self._sort_method = None
+        
+        def _set_sort_method(self, sort_field, sort_type):
+            self._sort_method = self.sort_fields[sort_field] + ':' + sort_type[sort_type]
+        
+        def return_sort(self):
+            if (self._sort_method is not None):
+                return 'sort='+ self._sort_method
+            
+            else:
+                APIAdaptor.warning_msg(2)            
+
+
+
     def api_syntax(self):
         # defining core api combination syntax
+        # search
         pass
 
     def api_former(self):
@@ -35,6 +114,7 @@ class APIRequest(APIAdaptor):
             pass
         else:
             pass
+
 
 
 #### Unit Test Script
--- src/PageParser.py
+++ src/PageParser.py
@@ -5,20 +5,125 @@ example source: https://www.accessdata.fda.gov/scripts/cdrh/cfdocs/cfpmn/pmn.cfm
 '''
 
 import urllib.request
+import requests
 from bs4 import BeautifulSoup as bs
 import re
-TEST = True
 
-if TEST:
+#### Params
+url = 'https://www.accessdata.fda.gov/scripts/cdrh/cfdocs/cfpmn/pmn.cfm?ID=K130878'
+
+keys = tuple([
+    "Device Classification Name",
+    "510(K) Number",
+    "Device Name",
+    "Applicant",
+    "Applicant Contact",
+    "Correspondent",
+    "Correspondent Contact",
+    "Regulation Number",
+    "Classification Product Code",
+    "Date Received",
+    "Decision Date",
+    "Decision",
+    "Regulation Medical Specialty",
+    "510k Review Panel",
+    "Summary",
+    "Type",
+    "Reviewed By Third Party",
+    "Combination Product"
+    ])
+
+r = requests.get(url)
+
+# Check Status
+r.status_code
+
+# Check encoding
+r.encoding
+
+# Check content
+text = r.text
+
+# Parse with BeautifulSoup
+soup = bs(text, 'lxml')
+
+# Retrieve Central Table Content
+table = soup.find('table', {'align': 'center'})
+keys = [th.text.replace('\n', '') for th in table.find('tr').find_all('th')]
+keys_2 = [th.text.replace('\n', '') for th in table.find('tr').find_all('th', {'align': 'left'} )]
+
+# trs = table.find_all('tr')[1:]
+# main_table = trs[3].find_all('tr')
+
+# content = dict() 
+# l_col = list()
+# r_col = list()
+# links = list()
+
+# last_key = ''
+# last_val = ''
+
+# for tr in main_table:
+#     curr_key = ' '.join([re.sub('[\n\t\r\xa0]', '', th.text) for th in tr.find_all('th', {'align': 'left'})])
+#     curr_val = ' '.join([re.sub('[\n\t\r\xa0]', '', td.text) for td in tr.find_all('td', {'align': 'left'}, recursive = False)])
+#     link = [l.get('href') for l in tr.find('td').find_all('a', href = True)]
+
+#     if len(link) != 0:
+#         links.append(link)
+#         l_str = ' | '.join(link)
+#         curr_val += (' | ' + (l_str))
+
+#     if (curr_key == ''):
+#         continue
+
+#     else:       
+#         l_col.append(curr_key)
+#         r_col.append(curr_val)
+
+# for i in range(len(l_col)):
+#     print(l_col[i], ' : ', r_col[i])
+
+if __name__ == "__main__":
     url = 'https://www.accessdata.fda.gov/scripts/cdrh/cfdocs/cfpmn/pmn.cfm?ID=K130878'
+    # url = 'https://www.accessdata.fda.gov/scripts/cdrh/cfdocs/cfpmn/pmn.cfm?ID=K091000'
     # Download from url
-    response = urllib.request.urlopen(url)
-    data = response.read()
-    text = data.decode('utf-8-sig')
+    r = requests.get(url)
 
-    # Parse from text
-    soup = bs(text, 'lxml')
+    # Parse with BeautifulSoup
+    soup = bs(r.text, 'lxml')
 
-    # Simple Re
-    tag = r'<tr><th align="left">'
-    re.findall(tag, text)
+    # Retrieve Central Table Content
+    table = soup.find('table', {'align': 'center'})
+    keys = [th.text.replace('\n', '') for th in table.find('tr').find_all('th')]
+
+    # Temp Try
+    trs = table.find_all('tr')[1:]
+    main_table = trs[3].find_all('tr')
+
+    content = dict() 
+    l_col = list()
+    r_col = list()
+    links = list()
+
+    last_key = ''
+    last_val = ''
+
+    for tr in main_table:
+        curr_key = ' '.join([re.sub('[\n\t\r\xa0]', '', th.text) for th in tr.find_all('th', {'align': 'left'})])
+        curr_val = ' '.join([re.sub('[\n\t\r\xa0]', '', td.text) for td in tr.find_all('td', {'align': 'left'}, recursive = False)])
+        link = [l.get('href') for l in tr.find('td').find_all('a', href = True)]
+
+        if len(link) != 0:
+            links.append(link)
+            l_str = ' | '.join(link)
+            curr_val += (' | ' + (l_str))
+
+        if (curr_key == ''):
+            continue
+
+        else:       
+            l_col.append(curr_key)
+            r_col.append(curr_val)
+
+    for i in range(len(l_col)):
+        print(l_col[i], ' : ', r_col[i])
