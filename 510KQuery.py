e3bbbaa5c6ff054e3ec1c56504a1f33ceb50d32e
lawrencechen <lawrencechen@ixensor.com>
2020-05-30 17:59:06 +0800

    Add loop searching

--- 510KQuery.py
+++ 510KQuery.py
@@ -13,6 +13,26 @@ from src import BatchQuery
 from src import PageParser
 from src.utils.Utils import FilePathHandler
 
+class Interact:
+    TYPE_LIST = ['terminal', 'web']
+    def __init__(self, type_num):
+        self._target = self.TYPE_LIST[type_num]
+
+    def emit(self, msg):
+        if (self._target == self.TYPE_LIST[0]):
+            print(msg)
+        elif (self._target == self.TYPE_LIST[1]):
+            pass
+    
+    def receive(self, msg):
+        if (self._target == self.TYPE_LIST[0]):
+            value = input(msg)
+            return value
+        elif (self._target == self.TYPE_LIST[1]):
+            pass
+
+
+#### Define Query
 class SingleQuery:
 # This class will be refactored to be a FSM
     PROCESS_LIST = {0:'Initialization', 1:'Argument Taking', 2:'Batch Query', 3:'Page Parsing', 4:'End Process', 5:'Exception'}
@@ -23,17 +43,17 @@ class SingleQuery:
     ROOT_URL = 'https://www.accessdata.fda.gov/scripts/cdrh/cfdocs/cfpmn/pmn.cfm?ID={}'
     DENOVO_URL = 'https://www.accessdata.fda.gov/scripts/cdrh/cfdocs/cfpmn/denovo.cfm?ID={}'
 
-
-    def _report(self):
-        print('{} Process'.format(self.STATUS))
-
-
-    def _set_process(self, curr_proc):
-        self.STATUS = self.PROCESS_LIST[curr_proc]
-        self._report()
+    class Args:
+        def __init__(self, args_dict):
+            for k, v in args_dict.items():
+                self.k = v
 
 
-    def __init__(self):
+    def __init__(self, interact_type = 0):
+        # set interact
+        self.interact = Interact(interact_type)
+
+        # members
         self._set_process(0)
         self._args_dict = {}
         self._k_num_list = []
@@ -44,6 +64,12 @@ class SingleQuery:
         self.path_handler.root = self.ROOT
         self.path_handler.curr_path = datetime.now().strftime('%Y%m%d-%H%M%S')
 
+    def _report(self):
+        self.interact.emit('{} Process'.format(self.STATUS))
+
+    def _set_process(self, curr_proc):
+        self.STATUS = self.PROCESS_LIST[curr_proc]
+        self._report()
 
     def ArgumentProcessTerminal(self):
         self._set_process(1)
@@ -67,16 +93,74 @@ class SingleQuery:
 
         self._args_dict = vars(args)
 
-        print('*'*45 + '\n\t\tInput Arguments\n' + '*'*45)
-        print('Search Key:\t', args.sr_key)
-        print('Search Product Code:\t', args.sr_pd_code, '(OR)' if args.bool_pd_code_or else '(AND)')
-        print('Search Regulation Number:\t', args.sr_rgl_num, '(OR)' if args.bool_rgl_num_or else '(AND)')
-        print('Search Time (From):\t', args.sr_time_f)
-        print('Search Time (To):\t', args.sr_time_t)
-        print('Search Number:\t', args.l_num)
-        print('Sort Type:\t', args.srt_type)
+        self.interact.emit('*'*45 + '\n\t\tInput Arguments\n' + '*'*45)
+        self.interact.emit('Search Key:\t{}'.format(args.sr_key))
+        self.interact.emit('Search Product Code:\t{} {}'.format(args.sr_pd_code, '(OR)' if args.bool_pd_code_or else '(AND)'))
+        self.interact.emit('Search Regulation Number:\t{} {}'.format(args.sr_rgl_num, '(OR)' if args.bool_rgl_num_or else '(AND)'))
+        self.interact.emit('Search Time (From):\t{}'.format(args.sr_time_f))
+        self.interact.emit('Search Time (To):\t{}'.format(args.sr_time_t))
+        self.interact.emit('Search Number:\t{}'.format(args.l_num))
+        self.interact.emit('Sort Type:\t{}'.format(args.srt_type))
 
 
+    def ArgumentProcess(self):
+        # Deal input
+        args = self.Args(self.ARGS)
+        args.sr_key = self.interact.receive('Enter Search Device Name: ')
+        args.sr_pd_code = self.interact.receive('Enter Search Product Code: ')
+        if (args.sr_pd_code != ''): 
+             args.bool_pd_code_or = bool(self.interact.receive('Search With Or?'))
+        else:
+            args.sr_pd_code = None
+            args.bool_pd_code_or = None
+        args.sr_rgl_num = self.interact.receive('Enter Regulation Number: ')
+        if (args.sr_rgl_num != ''):
+            args.bool_rgl_num_or = bool(self.interact.receive('Search With Or?'))
+        else:
+            args.sr_rgl_num = None
+            args.bool_rgl_num_or = None
+        args.sr_time_f = self.interact.receive('Enter Search Time (From): ')
+        args.sr_time_t = self.interact.receive('Enter Search Time (To)  : ')
+        args.l_num = self.interact.receive('Enter Files Numbers (Max. 200): ')
+        try:
+            val = int(args.l_num)
+            if val > 200 or val < 1:
+                args.l_num = 20
+                self.interact.emit('Wrong Input, using default.')
+            else:
+                args.l_num = val
+
+        except ValueError:
+            self.interact.emit('Wrong Input, using default.')
+            args.l_num = 20
+
+        args.srt_type = self.interact.receive('Enter Sort Type (1: Desc, 2 Asc): ')
+        try:
+            val = int(args.srt_type)
+            if val not in [1, 2]:
+                self.interact.emit('Wrong Input, using default.')
+                args.srt_type = 1
+            else:
+                args.srt_type = val
+
+        except ValueError:
+            self.interact.emit('Wrong Input, using default.')
+            args.srt_type = 1
+
+
+        self._args_dict = vars(args)
+
+        # Deal report
+        self.interact.emit('*'*45 + '\n\t\tInput Arguments\n' + '*'*45)
+        self.interact.emit('Search Key:\t{}'.format(args.sr_key))
+        self.interact.emit('Search Product Code:\t{} {}'.format(args.sr_pd_code, '(OR)' if args.bool_pd_code_or else '(AND)'))
+        self.interact.emit('Search Regulation Number:\t{} {}'.format(args.sr_rgl_num, '(OR)' if args.bool_rgl_num_or else '(AND)'))
+        self.interact.emit('Search Time (From):\t{}'.format(args.sr_time_f))
+        self.interact.emit('Search Time (To):\t{}'.format(args.sr_time_t))
+        self.interact.emit('Search Number:\t{}'.format(args.l_num))
+        self.interact.emit('Sort Type:\t{}'.format(args.srt_type))
+        
+        
     def BatchQueryProcess(self):
         self._set_process(2)
 
@@ -90,23 +174,23 @@ class SingleQuery:
                         sort_type = self._args_dict['srt_type'])
         root_url  = batch_query.return_final_query()
         self._quesy_list = batch_query.return_query_list()
-        print('API Query:\t', root_url)
+        self.interact.emit('API Query:\t{}'.format(root_url))
 
         # Request from database
-        print('*'*45 + '\n\t\tRequest From Database\n' + '*'*45)
+        self.interact.emit('*'*45 + '\n\t\tRequest From Database\n' + '*'*45)
         
         data = requests.get(root_url)
-        print('Connection Success:\t{}'.format(data.status_code == 200))
+        self.interact.emit('Connection Success:\t{}'.format(data.status_code == 200))
 
         # read to json & output
         json_data = json.loads(data.text)
 
         if (data.status_code != 200):
             # raise ConnectionError('Error: ', json_data['error']['message'])
-            print('Error: ', json_data['error']['message'])
+            self.interact.emit('Error: {}'.format(json_data['error']['message']))
             self.EndProcess()
 
-        print('Return Files / Total:\t {} / {}'.format(json_data['meta']['results']['limit'], json_data['meta']['results']['total']))
+        self.interact.emit('Return Files / Total:\t {} / {}'.format(json_data['meta']['results']['limit'], json_data['meta']['results']['total']))
 
         # to data
         df = DataFrame(json_data['results'])
@@ -118,8 +202,8 @@ class SingleQuery:
         if (save is 'Y') or (save is 'y'):
             file_name = self.path_handler.curr_path + '{}-database.csv'.format(self._quesy_list)
             df.to_csv(file_name)
-            print('Saved.')
-        print('Overview:\n', df.head())
+            self.interact.emit('Saved.')
+        self.interact.emit('Overview:\n {}'.format(df.head()))
         del df
 
     def PageParsingProcess(self):
@@ -136,12 +220,11 @@ class SingleQuery:
             key_dict = page_parser.pdf_dealer(self.path_handler.curr_path, pdf_search_key)
 
             dict_list.append(page_parser.element_dict)
-            print('Status: {} | | {} | Number of PDF: {} | Containing Key {}'.format(
+            self.interact.emit('Status: {} | | {} | Number of PDF: {} | Containing Key: {}'.format(
                 'OK' if (page_parser.get_url_data.status_code == 200) else 'Fail',
                 u,
                 len(page_parser.return_pdf_links_dict()),
-                'Yes' if len(key_dict) > 0 else 'No'
-            ))
+                'Yes' if len(key_dict) > 0 else 'No'))
 
         # save?
         save = input('Save parsed results into CSV? (Y/N): ')
@@ -150,19 +233,27 @@ class SingleQuery:
         if (save is 'Y') or (save is 'y'):
             file_name = self.path_handler.curr_path + '{}-parsed.csv'.format(self._quesy_list)
             df.to_csv(file_name)
-            print('Saved.')
-        print('Overview: ', df.head())
+            self.interact.emit('Saved.')
+        self.interact.emit('Overview: {}'.format(df.head()))
         del df
     
     def EndProcess(self):
-        print('Bye bro.')
+        self.interact.emit('Bye bro.')
         exit()
     
 
-
 if __name__ == '__main__':
-    a = SingleQuery()
-    a.ArgumentProcessTerminal()
-    a.BatchQueryProcess()
-    a.PageParsingProcess()
-    input('Exit Programe.')
+
+    while(True):
+        a = SingleQuery()
+        try:
+            a.ArgumentProcess()
+            a.BatchQueryProcess()
+            a.PageParsingProcess()
+        except:
+            print('Encounter Unexpected Error. Please search again.')
+            continue
+
+        if not bool(input('Continue? Press "Enter" to leave, "ok" to conitnue: ')):
+            a.EndProcess()
+            break
